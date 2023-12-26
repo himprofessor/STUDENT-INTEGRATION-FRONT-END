97337ec4c32b6d3c64e400a36650c921aafb9170
SREYPHEAP.KHIN <SREYPHEAP.KHIN@PNC.LAN>
2023-12-26 15:09:00 +0700

    [fetch data of staff]

---
+++ src/views/staff/StaffView.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="flex flex-wrap justify-between mx-20">
+        <StaffComponent 
+            v-for="(staff,index) in staffs" 
+            :key="index"
+            :staff="staff"
+        />
+    </div>
+</template>
+
+<script>
+import StaffComponent from '@/components/staff/StaffComponent.vue'
+import http from "@/http-common"
+
+export default {
+    components:{
+        StaffComponent
+    },
+    data(){
+        return {
+            staffs:[],
+        };
+    },
+    methods: {
+        fetchData(){
+            http
+                .get("api/staff-list/list")
+                .then((response) =>{
+                    this.staffs = response.data.data;
+                })
+                .catch((error) =>{
+                    console.log("Error fetching dasta:", error);
+                });
+        },
+    },
+    mounted(){
+        this.fetchData();
+    },
+}
+</script>
+
+<style>
+
+</style>
