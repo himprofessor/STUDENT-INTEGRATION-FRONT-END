fc7f030eca901e2ef51166df2f8f42fef09372c2
dariyathorn <dariya.thorn@student.passerellesnumeriques.org>
2023-12-26 15:32:17 +0700

    [create folder footer component]

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,19 @@
   <CardView />
   <StaffComponent/>
   <MapComponent/>
+  <FooterComponent/>
 </template>
 <script>
+import FooterComponent from "./components/footer/FooterComponent.vue";
 import StaffComponent from "./components/staff/StaffComponent.vue";
 import CardView from './views/card/CardView.vue'; // 
 import HeaderComponent from "./components/header/HeaderComponent.vue";
 import NavbarComponent from "./components/header/NavbarComponent.vue";
 import SlideshowComponent from './components/slideshow/SlideshowComponent.vue';
 import MapComponent from './components/map/MapComponent.vue';
-
 export default {
   components: {
+    FooterComponent,
     StaffComponent,
     SlideshowComponent,
     MapComponent,
