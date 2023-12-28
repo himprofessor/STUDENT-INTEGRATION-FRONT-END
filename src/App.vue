86d31435c784018512db97a6c58e748f960bf2ae
dariyathorn <dariya.thorn@student.passerellesnumeriques.org>
2023-12-28 17:34:23 +0700

    [Create card of internship]

--- src/App.vue
+++ src/App.vue
@@ -4,24 +4,27 @@
   <SlideshowComponent />
   <CardView />
   <StaffView/>
-  <!-- <MapComponent/> -->
+  <MapComponent/>
+  <CareerView/>
   <FooterView/>
 </template>
 
 <script>
+import CareerView from "./views/career/CareerView.vue";
 import FooterView from "./views/footer/FooterView.vue";
 import StaffView from './views/staff/StaffView.vue';
-import CardView from './views/card/CardView.vue'; // 
+import CardView from './views/card/CardView.vue'; 
 import HeaderComponent from "./components/header/HeaderComponent.vue";
 import NavbarComponent from "./components/header/NavbarComponent.vue";
 import SlideshowComponent from './components/slideshow/SlideshowComponent.vue';
-// import MapComponent from './components/map/MapComponent.vue';
+import MapComponent from './components/map/MapComponent.vue';
 export default {
   components: {
+    CareerView,
     SlideshowComponent,
-    // MapComponent,
+    MapComponent,
     StaffView,
-    CardView, //
+    CardView,
     HeaderComponent,
     NavbarComponent,
     FooterView,
