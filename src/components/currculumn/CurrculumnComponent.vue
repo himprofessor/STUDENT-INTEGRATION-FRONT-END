441ddd5713429c814c4080004374ebf1b5b0c596
dariyathorn <dariya.thorn@student.passerellesnumeriques.org>
2023-12-29 09:54:34 +0700

    [update style image]

--- src/components/currculumn/CurrculumnComponent.vue
+++ src/components/currculumn/CurrculumnComponent.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="flex flex-col justify-center md:h-full mt-5 ">
+        <h1 class="lg:text-4xl md:text-3xl text-lg font-bold lg:my-6 md:my-5 mx-20">Curriculum</h1>
         <div v-for="(currculum, index) in currculums" :key="index" class="relative flex flex-col md:flex-row md:space-x-3 space-y-3 md:space-y-0 md:mx-20 mx-5 mt-3 rounded-lg">
             <div class="md:w-1/5 grid md:place-items-center w-full">
-                <img :src="require(`../../assets/currculumn/${currculum.image}`)" alt="currculum logo" class="md:h-32 md:w-1/2 w-96 h-48 my-3" />
+                <img :src="require(`../../assets/currculumn/${currculum.image}`)" alt="currculum logo" class="md:h-32 md:w-2/2 w-96 h-48 my-3" />
             </div>
             <div class="md:w-5/6 bg-white flex flex-col space-y-2 md:p-3 font-nunito md:text-start pb-3">
                 <h3 class="font-bold text-gray-800 md:text-3xl text-lg" >
@@ -11,6 +12,27 @@
                 <p class="md:text-base text-gray-500 text-sm">
                     {{currculum.description}}
                 </p>
+                <span
+        @click="readMore(currculum)"
+        class="text-blue-500 rounded flex items-center cursor-pointer mt-3"
+        >
+        Read more
+        <svg
+            class="h-5 w-5 text-blue-500"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            stroke-width="2"
+            stroke="currentColor"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        >
+            <path stroke="none" d="M0 0h24v24H0z" />
+            <polyline points="7 7 12 12 7 17" />
+            <polyline points="13 7 18 12 13 17" />
+        </svg>
+        </span>
             </div>
         </div>
     </div>
