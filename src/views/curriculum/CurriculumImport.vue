9af3fd188ad26ef1f5b6101ed28218881a420ba9
SOPHORN.TOEUM <SOPHORN.TOEUM@PNC.LAN>
2024-01-29 09:51:13 +0700

    [TASK] create new Curriculum

---
+++ src/views/curriculum/CurriculumImport.vue
@@ -0,0 +1,25 @@
+<template>
+    <CurriculumView/>
+    <SubjectComponent/>
+    <TermsComponent/>
+    <InternShipView/>
+    <ImpactComponent/> 
+</template>
+
+<script>
+import CurriculumView from './CurriculumView.vue';
+import TermsComponent from '@/components/term/TermsComponent.vue';
+import SubjectComponent from '@/components/subject/SubjectComponent.vue';
+import InternShipView from '../internship/InternShipView.vue';
+import ImpactComponent from '@/components/impact/ImpactComponent.vue';
+
+export default{
+    components:{
+        CurriculumView,
+        TermsComponent,
+        SubjectComponent,
+        InternShipView,
+        ImpactComponent
+    }
+}
+</script>
