0b75259457599e80fe602620a03c56dbb415723e
SOPHORN.TOEUM <SOPHORN.TOEUM@pnc.lan>
2024-02-23 07:42:40 +0700

    [TASK] update style in partnership

--- src/components/partnership/PartnershipComponent.vue
+++ src/components/partnership/PartnershipComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div @mouseenter="activeIndex = index" @mouseleave="activeIndex = null"
     class="flex items-center cursor-pointer justify-center lg:w-72 md:w-[32%] w-full lg:h-52 md:h-52 h-72 rounded-md border-2 relative">
-    <img :src="partnership.media_id" alt="partnership" class="w-40 h-40 rounded-full" />
+    <img :src="partnership.media_id" alt="partnership" class="w-40 h-40"/>
     <!-- hover text  -->
     <div v-if="activeIndex === index" class="hover:border-blue-500 rounded-md border-2 absolute inset-0 bg-black bg-opacity-50 text-center text-white flex flex-col justify-center">
       <span class="text-3xl font-semibold">{{ partnership.partnership_name }}</span>
